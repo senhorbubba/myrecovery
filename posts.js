5e214675da7c327132b1e23c95bbaabd44a2c13f
senhorbubba <dczarcin@gmail.com>
2026-03-03 14:35:36 -0300

    Add post: Dia 103: Ritmo Acelerado, Reset Forçado

--- posts.js
+++ posts.js
@@ -1,6 +1,16 @@
 const posts = [
  {
         tag: "Semana 15",
+        date: "28 Fev, 2026",
+        pain: "2/10",
+        swelling: "2/10",
+        title: "Dia 103: Ritmo Acelerado, Reset Forçado",
+        description: "Um dia que correu rápido demais e exigiu além da conta. O estresse foi se acumulando silenciosamente entre trabalho e falta de ritmo, mas um breve momento na quadra com as crianças mudou tudo. Trinta minutos de movimento restauraram o que horas de pressão drenaram. O joelho respondeu bem. A mente desacelerou. Hoje, a recuperação foi menos física e mais emocional.",
+        image: "images/Day103.jpg",
+        link: "posts/day-103.html"
+    },
+ {
+        tag: "Semana 15",
         date: "27 Feb, 2026",
         pain: "1/10",
         swelling: "2/10",
